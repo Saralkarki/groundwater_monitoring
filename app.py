b97964e1b4e2ce30ed26d92699703da4c46b1936
saral karki <saral.karki@gmail.com>
2020-12-09 09:05:22 +0545

    login authentication

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import dash
 
 
+
+
 external_stylesheets = ['assets/css/main.css','assets/css/user.css' ]
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 app.config.suppress_callback_exceptions = True
--- index.py
+++ index.py
@@ -3,14 +3,28 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-
+import dash_auth
 
 from app import app
 from layouts import main_layout, pilot_layout
 import callbacks
 
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'admin': 'admin@123',
+    'administrator': 'admin@123'
+}
+
+
+
+
+
 
 server = app.server
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
